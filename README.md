54e45180eab8a6c278d8e65f7b2ed386668de7c0
mohsaide <https://github.com/mohsaide>
2022-04-05 01:34:49 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+#MAS
+
+title : portfolio
+start date : March 30 , 2022
+using technolgies : html , css , js , media query 
+
+description : 
+
+i try to define my identity in this website , 
+
+
+________________________________________________________
+contact : 
+
+gmail : mohammad2001saide@gmail.com 
+linkedin : https://www.linkedin.com/in/mohammadasaide/
+facebook : https://www.facebook.com/mhmd.a.saide
+
+
+thanks for read it ,
+🤍
---
+++ assests/js/do.js
@@ -0,0 +1,14 @@
+
+    function job_tv() {
+      document.getElementById("tv").style.backgroundImage = "url('/assests/img/form-harri.jpeg')" ;
+    }
+
+    function freelance_tv() {
+        document.getElementById("tv").style.backgroundImage = "url('/assests/img/form-coderizer.jpeg')" ;
+      }
+
+      function edu_tv() {
+        document.getElementById("tv").style.backgroundImage = "url('/assests/img/form-university.jpeg')" ;
+      }
+
+    
